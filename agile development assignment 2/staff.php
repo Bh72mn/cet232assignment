a8c52a4017bac5dbdaf567e62579a0f6f0339843
Bh72mn <130706435+Bh72mn@users.noreply.github.com>
2024-01-23 15:09:02 +0000

    Update staff.php
    Data has been inputted

--- agile development assignment 2/staff.php
+++ agile development assignment 2/staff.php
@@ -5,5 +5,23 @@ CREATE TABLE staff(
    ...> job TEXT NOT NULL)
    ...> ;
    
-      INSERT INTO staff(id, name, password, job) VALUES(0384, 'Scott', 'password', 'instore');
-   
+  INSERT INTO staff(id, name, password, job) VALUES(0384, 'Scott', 'password', 'instore');
+  INSERT INTO staff(id, name, password, job) VALUES(1108, 'Louise', 'password', 'driver');
+  INSERT INTO staff(id, name, password, job) VALUES(8752, 'Emily', 'password', 'shiftrunner');
+  INSERT INTO staff(id, name, password, job) VALUES(2098, 'Mel', 'password', 'manager');
+  INSERT INTO staff(id, name, password, job) VALUES(3120, 'Chris', 'password', 'instore');
+  INSERT INTO staff(id, name, password, job) VALUES(7711, 'Edward', 'password', 'driver');
+  INSERT INTO staff(id, name, password, job) VALUES(9012, 'Craig', 'password', 'driver');
+  INSERT INTO staff(id, name, password, job) VALUES(8790, 'Dylan', 'password', 'driver');
+  INSERT INTO staff(id, name, password, job) VALUES(7854, 'Kurt', 'password', 'instore');
+  INSERT INTO staff(id, name, password, job) VALUES(1134, 'Kieron', 'password', 'insrore');
+  INSERT INTO staff(id, name, password, job) VALUES(6632, 'Harry', 'password', 'driver');
+  INSERT INTO staff(id, name, password, job) VALUES(2134, 'Rosie', 'password', 'driver');
+  INSERT INTO staff(id, name, password, job) VALUES(9065, 'Elizabeth', 'password', 'instore');
+  INSERT INTO staff(id, name, password, job) VALUES(1111, 'Rebecca', 'password', 'Headoffice');
+  INSERT INTO staff(id, name, password, job) VALUES(2234, 'James', 'password', 'instore');
+  INSERT INTO staff(id, name, password, job) VALUES(1155, 'John', 'password', 'driver');
+  INSERT INTO staff(id, name, password, job) VALUES(9771, 'Jessica', 'password', 'driver');
+  INSERT INTO staff(id, name, password, job) VALUES(6680, 'Kane', 'password', 'driver');
+  INSERT INTO staff(id, name, password, job) VALUES(1129, 'Rachel', 'password', 'instore');
+  INSERT INTO staff(id, name, password, job) VALUES(1234, 'Ben', 'password', 'driver');
